51c46fedd89c121dbe1b14e93bf8f5e5369babc3
Mohi Uddin Anando <64367534+Mohi-Anando@users.noreply.github.com>
2022-10-28 16:13:46 +0600

    completed

---
+++ lab task 3/propic.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  <img src="Image/pp.png" alt="" height="100px" width="100px">
+  <br>
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
